0d7fb699399da73c45502815671c1a35cf14569f
Enthernet Code <94282152+Enthernetcode@users.noreply.github.com>
2022-09-07 07:02:43 +0100

    Update passcrack.py

--- passcrack.py
+++ passcrack.py
@@ -3,7 +3,7 @@ numbers = '1234567890abcdefghijklmnopqrstivwxyz'
 chars = 'abc123AB,C.!'
 guess = ''
 pin = ''
-guess = input('')
+guess = input('Enter a length of 4')
 while pin != guess:
 	pin = random.choices(numbers,k=4)
 	pin = ''.join(pin)
